537daf2c2740a48c6dbfd87d6b724a03a6867b60
Filip Tchorek <filip.tchorek@gmail.com>
2017-12-30 15:56:07 +0100

    Add jshint task runner

---
+++ js/scripts.js
@@ -0,0 +1,96 @@
+// THIS IS DOGESCRIPT 
+
+/* 
+    dogescript was created by Zach Bruggeman
+    and has been improved by many contributors
+    such appreciate, much thanks, wow
+*/
+
+"use strict";
+
+var dogeudle = require('dogeudle');
+var wow = require('boring');
+
+var dogescript = 'such messy; very doge-friendly';
+
+function woof(foo, bar, bat) {
+    console.log(foo);
+
+    function nested() {
+        console.log(['so', 'wow']);
+    }
+    nested();
+    return bar;
+}
+
+woof('multiple', 'doge', 'properties wow');
+
+var cat = 'not a doge';
+
+if (cat === 'doge') {
+    console.log('wow, such liar, much cat');
+} else if (cat === 'not a doge') {
+    console.log('very truth, such belief, wow');
+} else {
+    console.log('such impossible');
+}
+
+while (woof === 'doges only' && cat !== 'doge' || cat === 'not a doge') {
+    console.log({
+        such: 'doge'
+    });
+}
+
+if (woof === 'doges only' && cat !== 'doge' || cat === 'not a doge') {
+    console.log({
+        such: 'doge',
+        wow: 'dogee'
+    });
+}
+
+for (var woof = 1; woof < 3; woof += 1) {
+    console.log({
+        such: 'doge'
+    });
+}
+
+// doge style 
+var science = 4;
+for (var so = 1; so < 10; so += 1) {
+    if (so > 2) {
+        console.log({
+            such: so
+        });
+    }
+}
+
+
+// chaining 
+var canvas = d3.select('body')
+    .append('canvas')
+    .attr('width', width)
+    .attr('height', height);
+
+var regex = new RegExp(keyword, 'g');
+var regex2 = new RegExp('doge', 'g');
+
+var arr = new Array(0);
+
+obj = new Object();
+
+module.exports = woof
+
+// example http server 
+var http = require('http');
+http.createServer(function(req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    res.end('so hello\nmuch world');
+})
+    .listen(8080);
+
+window.doge = 'so global, no scope wow'
+var dogelement = document.createElement('doge');
+
+( !! +Math.round(Math.random()))
